77758437b1244439488488b7b5bf5d97f301b60e
Jacob Widaeus <jacob.widaeus@gmail.com>
2024-04-04 16:12:44 +0200

    Added Roxygen2 to 2nd function.

--- R/importfunctions.R
+++ R/importfunctions.R
@@ -1,11 +1,9 @@
-
-#' xml_to_df
+#' xml_to_singlerow
 #'
 #' @description
 #' *Applicable to microvasc research group, Danderyds Sjukhus workflow*. Takes a PimSOFT report in XML format and generates a single row of variables suited for implementation into a data frame. It is suited for reports generated with "Recording info" and "Mean perfusion" boxes ticked.
 #'
 #' @param xml_file_path file path to xml file in working directory
-#' @param xml_folder_file_path file path to folder with xml files in working directory
 #'
 #' @return returns
 #' @export
@@ -121,6 +119,18 @@ xml_to_singlerow <- function(xml_file_path) {
   return(data)
 }
 
+
+
+#' singlerow_df_compile
+#'
+#'#' @description
+#' *Applicable to microvasc research group, Danderyds Sjukhus workflow*. Takes a *FOLDER* of PimSOFT report in XML format and generates a single row of variables suited for implementation into a data frame. It is suited for reports generated with "Recording info" and "Mean perfusion" boxes ticked.
+#'
+#' @param xml_folder_file_path file path to folder that needs to be compiled.
+#'
+#' @return returns
+#' @export
+#'
 singlerow_df_compile <- function(xml_folder_file_path) {
   # List all XML files in the target directory
   files <- list.files(path = xml_folder_file_path, pattern = "\\.xml$", full.names = TRUE)
