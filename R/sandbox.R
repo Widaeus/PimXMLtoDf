2cbe50fe907cde166ca1650a7caa9baa97311738
Jacob Widaeus <jacob.widaeus@gmail.com>
2024-04-05 15:18:47 +0200

    Appended directed if else chain for cleaning.

--- R/sandbox.R
+++ R/sandbox.R
@@ -8,23 +8,18 @@ meanperf <- "sandbox/CV01_dag1_meanperf.xml"
 meanperf_recinfo <- "sandbox/CV01_dag1_meanperf_recinfo.xml"
 meanperf_recinfo_calculations <- "sandbox/CV01_dag1_meanperf_recinfo_calculations.xml"
 meanperf_recinfo_calculations_percentchangeroi <- "sandbox/CV01_dag1_meanperf_recinfo_calculations_percentchangeroi.xml"
-recordinginfo = TRUE
-calculations = TRUE
-meanperfusion = TRUE
+# Enter perameters #
+recordinginfo = FALSE
+calculations = FALSE
+meanperfusion = FALSE
 changeperROI = FALSE
-
-#testing with meanperf + recinfo
-test <- xml_to_singlerow(meanperf_recinfo)
-view(test)
-
-
 
 
 #######evaluating code
 ns <- c(ss = "urn:schemas-microsoft-com:office:spreadsheet")
 
 # Read the XML file
-xml_data <- read_xml(meanperf_recinfo_calculations)
+xml_data <- read_xml(meanperf_recinfo_calculations_percentchangeroi)
 
 # Find all rows in the document
 rows <- xml_find_all(xml_data, ".//ss:Row", ns = ns)
@@ -36,30 +31,32 @@ data <- map_df(rows, ~{
   tibble(cell_data)
 })
 
-# Cleaningprocess based on XML output### in progress
-if (identical(meanperfusion, TRUE)) {
-  data <- data[-1, ]
-} else if (recordinginfo) {
+# Remove empty strings
+data <- data %>%
+  filter(!if_any(everything(), ~ .x == ""))
 
+
+# Cleaningprocess based on XML output### in progress
+if (identical(meanperfusion, TRUE) &
+    identical(recordinginfo | calculations | changeperROI, FALSE)) {
+  data <- data[-1, ]
+
+} else if (identical(meanperfusion & recordinginfo, TRUE) &
+           identical(calculations | changeperROI, FALSE)) {
+  data <- data[-c(1:2, 4:6, 8:14), ]
+
+} else if (identical(meanperfusion & recordinginfo & calculations, TRUE) &
+           identical(changeperROI, FALSE)) {
+  data <- data %>%
+    filter(!if_any(everything(), ~grepl("Mean Perfusion", .x)))
+  data <- data[-c(1:2, 4:6, 8:14), ]
+
+} else if (identical(meanperfusion & recordinginfo & calculations & changeperROI, TRUE)) {
+  data <- data %>%
+    filter(!if_any(everything(), ~grepl("Percent Change Per ROI", .x)))
 }
 
-#######recinfo what to remove
-data <- data[-c(1:2, 4:6, 8:13),]
-
-######## meanperf what to remove
-data <- data[-1, ]
-
-####### calculations what to remove
-data <- data[-c(3:5, 12:18, 25, 32, 39, 47:53, 60, 67, 74, 82:88, 95, 102, 109, 117:123),]
-
-### ORDER
-# RECINFO -> Calculations -> meanperfusion
 
 # Extract the column as a vector
-original_data <- data$cell_data
-
-original_data_clean <- original_data[-(1:12)]
-
-
-data
+original_data <- filtered_data$cell_data
 
